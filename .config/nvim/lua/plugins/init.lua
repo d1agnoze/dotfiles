763ff209250eb48d2173f76412d8f4b8d242bd88
vdac <dacvuong202@gmail.com>
2024-03-30 11:38:58 +0700

    FIX: colorizer not showing up

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -144,4 +144,16 @@ return {
       require "vdac.todo"
     end,
   },
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        mode = "background", -- You can change the mode to "foreground" to colorize foreground color codes
+        RGB = true, -- Enable RGB color notation support
+        RRGGBB = true, -- Enable RRGGBB color notation support
+        names = false, -- Disable color name highlighting (e.g., "red", "blue", etc.)
+        css = true, -- Disable CSS color notation support
+      },
+    },
+  },
 }

c3deb71e3d56614c7a075ac072f70b36361a69f7
vdac <dacvuong202@gmail.com>
2024-07-25 18:45:20 +0700

    Changes: Add Fedora install script, update nvim plugins

---
+++ install_scripts/fedora.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Installing programs...."
+sudo dnf update
+sudo dnf install -y tmux
+sudo dnf install -y gcc make ripgrep cmake
+sudo dnf install -y python3 python3-pip
+sudo dnf install -y neofetch ranger
+sudo dnf install -y fzf zip unzip
+sudo dnf install -y highlight grc fuse
+

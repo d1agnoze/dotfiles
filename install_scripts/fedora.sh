37626c969477dea4fc859fe71c42ebac81be0ff0
Vdac <63735689+d1agnoze@users.noreply.github.com>
2025-04-12 17:01:35 +0700

    update install deps

--- install_scripts/fedora.sh
+++ install_scripts/fedora.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Installing programs...."
 sudo dnf update -y
-sudo dnf install -y stow tmux ripgrep python3-pip ranger fzf highlight grc fuse stow btop go neovim python3-neovim kitty git-delta meld zoxide
+sudo dnf install -y stow tmux ripgrep python3-pip ranger fzf highlight grc fuse stow python3-neovim kitty git-delta meld zoxide fastfetch

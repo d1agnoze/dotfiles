3f9017adc183250a854751524df7d36068599c67
vdac <dacvuong202@gmail.com>
2026-04-06 08:39:08 +0700

    update

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -12,7 +12,7 @@ M.base46 = {
     TelescopeSelection = { bg = "#045215", fg = "white" },
   },
   transparency = false,
-  theme = "nightfox",
+  theme = "everblush",
 }
 M.nvdash = {
   load_on_startup = true,

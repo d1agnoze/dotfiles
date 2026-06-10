1e584c69db61bbe10db1a9d7464f8522b48d6d9b
vdac <dacvuong202@gmail.com>
2026-06-10 18:12:48 +0700

    update

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -234,6 +234,13 @@ return {
       end,
     },
   },
+  {
+    "kevinhwang91/nvim-ibus-sw",
+    event = "InsertEnter",
+    config = function()
+      require("ibus-sw").setup()
+    end,
+  },
   -- Lazy.nvim
   {
     "olimorris/codecompanion.nvim",
